24374dac4a0fa1666477e9c622a920c97a8a7d8f
Daniel Que <dmdque@gmail.com>
2018-05-25 22:06:05 -0300

    Add advertise template

--- contracts/contracts/Kimono.sol
+++ contracts/contracts/Kimono.sol
@@ -1,12 +1,34 @@
 pragma solidity ^0.4.23;
 
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
 
 contract Kimono {
+  using SafeMath for uint256;
+
+  struct Revealer {
+    bytes32 publicKey;
+    uint256 minReward;
+    uint256 stake;
+  }
+
+  mapping(address => Revealer) revealerTable;
+  address[] revealers;
+  address kimonoToken;
 
   // CONSTRUCTOR
 
   constructor () public {
 
+  }
+
+
+  function advertise() public {
+    // take stake
+  }
+
+  function cancel() public {
   }
 
   // FALLBACK FUNCTION
