7eedcbfcbec66909e1825c6fca3f308d322514e8
Feridun Mert Celebi <mertcelebi93@gmail.com>
2018-05-26 00:08:43 -0300

    Add submitRevealSecret function

--- contracts/contracts/KimonoCoin.sol
+++ contracts/contracts/KimonoCoin.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.23;
 
-import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 
 contract KimonoCoin is MintableToken, BurnableToken {
