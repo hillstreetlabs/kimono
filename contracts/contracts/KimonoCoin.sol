3e9bac8451eb1301a9ba61f28547cbbaf6a606f4
Feridun Mert Celebi <mertcelebi93@gmail.com>
2018-05-25 21:45:13 -0300

    Push first, simple version of KimonoCoin + initial deploy script

---
+++ contracts/contracts/KimonoCoin.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
+
+
+contract KimonoCoin is MintableToken, BurnableToken {
+  string constant public name = "Kimono Coin"; // solium-disable-line uppercase
+  string constant public symbol = "OPEN"; // solium-disable-line uppercase
+  uint8 constant public decimals = 18; // solium-disable-line uppercase
+
+  uint256 constant MAX_UINT = 2**256 - 1;
+
+  // EVENTS
+
+  event ApproveAll(address sender, address spender);
+  event TestingDistributeTokens(address account, uint256 amount);
+
+
+  // CONSTRUCTOR
+
+  constructor () public {
+
+  }
+
+  // FALLBACK function
+
+  // Do nothing. Intentionally not payable.
+  function() public {
+
+  }
+
+  // PUBLIC FUNCTIONS
+
+  function approveAll(address _spender) public returns (bool) {
+    allowed[msg.sender][_spender] = MAX_UINT;
+    emit ApproveAll(msg.sender, _spender);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint _value)
+    public
+    returns (bool)
+  {
+    uint allowance = allowed[_from][msg.sender];
+    require(balances[_from] >= _value && allowance >= _value && balances[_to] + _value >= balances[_to]);
+    balances[_to] += _value;
+    balances[_from] -= _value;
+    if (allowance < MAX_UINT) {
+      allowed[_from][msg.sender] -= _value;
+    }
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+  // FUNCTIONS FOR TESTING PURPOSES
+
+  function distributeTokensForTesting(address[] _accounts, uint256 _amount)
+    public
+    onlyOwner
+    canMint
+    returns (bool)
+  {
+    for (uint i = 0; i < _accounts.length; i++) {
+      address to = _accounts[i];
+      mint(to, _amount);
+      emit TestingDistributeTokens(to, _amount);
+    }
+    return true;
+  }
+}
---
+++ contracts/migrations/2_deploy_contracts.js
@@ -0,0 +1,42 @@
+const KimonoCoin = artifacts.require("KimonoCoin");
+const Kimono = artifacts.require("Kimono");
+
+RINKEBY_ACCOUNTS = [
+  "0x8aa4b7c4B26a923cFA86251BE9Acfc834463D85E",
+  "0x5b310417Cb579ED9c4790a548ac5c38550f75B5B",
+  "0x88959364Ec2Fb18b723572D9ae9163c6fC4739a8",
+  "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+  "0xb4B2d6632bbd18969cCCCd3B9637f4323929E080"
+];
+
+module.exports = function(deployer, network, accounts) {
+  testAccounts = deployer.network_id === 4 ? RINKEBY_ACCOUNTS : accounts;
+  testEnvironment = deployer.network_id !== 1;
+
+  console.log("Deploying KimonoCoin...");
+  return deployer
+    .deploy(KimonoCoin)
+    .then(() => {
+      return KimonoCoin.deployed();
+    })
+    .then(t => {
+      token = t;
+
+      // Distribute tokens to the list of addresses for testing
+      if (testEnvironment) {
+        console.log("Minting test KimonoCoins for the accounts...");
+        const amount = new web3.BigNumber(web3.toWei(1000, "ether"));
+        return token.distributeTokensForTesting(testAccounts, amount);
+      }
+    })
+    .then(() => {
+      console.log("Deploying the Kimono protocol...");
+      return deployer.deploy(Kimono);
+    })
+    .then(() => {
+      return Kimono.deployed();
+    })
+    .then(() => {
+      console.log("Deployment complete!");
+    });
+};
