081780e8774b9e3c64646defe85563a2270eb3f0
Graham Kaemmer <graham.kaemmer@gmail.com>
2018-05-26 10:56:28 -0300

    Import contract into revealer

--- cli/src/Revealer.ts
+++ cli/src/Revealer.ts
@@ -1,13 +1,23 @@
 import Ethstream from "ethstream";
 import HttpProvider from "ethjs-provider-http";
 import { Block } from "ethjs-shared";
+import Eth from "ethjs-query";
+import EthContract from "ethjs-contract";
+import BN from "bn.js";
+import kimono from "../../contracts/build/contracts/Kimono.json";
 
 import * as crypto from "./util/crypto";
+
+interface KimonoContract {
+  advertise: (stakeAmount: BN, opts?: any) => Promise<string>;
+}
 
 export default class Revealer {
   privateKey: Uint8Array;
   publicKey: Uint8Array;
   ethstream: Ethstream;
+  eth: Eth;
+  contract: KimonoContract;
 
   constructor(privateKey: string, provider: HttpProvider) {
     this.privateKey = crypto.hexToBytes(privateKey);
@@ -16,6 +26,15 @@ export default class Revealer {
       onAddBlock: block => this.onAddBlock(block),
       onConfirmBlock: block => this.onConfirmBlock(block)
     });
+    this.eth = new Eth(provider);
+    this.contract = EthContract(this.eth)<KimonoContract>(kimono.abi).at(
+      process.env.CONTRACT_ADDRESS
+    );
+  }
+
+  async signup(stakeAmount: BN) {
+    await this.contract.advertise(stakeAmount);
+    // Send a registration to the contract with this private key
   }
 
   start() {
@@ -27,8 +46,8 @@ export default class Revealer {
   }
 
   async exit() {
-    await this.ethstream.stop();
     console.log("Exiting...");
+    await this.ethstream.stop();
   }
 
   onAddBlock(block: Block) {
