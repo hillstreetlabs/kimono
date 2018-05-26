ab039a75d0eddc451ae3ac38a20b975fe34c75b6
Graham Kaemmer <graham.kaemmer@gmail.com>
2018-05-26 00:57:24 -0300

    Fix provider stuff

--- cli/src/Revealer.ts
+++ cli/src/Revealer.ts
@@ -1,5 +1,5 @@
 import Ethstream from "ethstream";
-import EthjsHttpProvider from "ethjs-provider-http";
+import HttpProvider from "ethjs-provider-http";
 import { Block } from "ethjs-shared";
 
 import * as crypto from "./util/crypto";
@@ -9,10 +9,9 @@ export default class Revealer {
   publicKey: Uint8Array;
   ethstream: Ethstream;
 
-  constructor(privateKey: string) {
+  constructor(privateKey: string, provider: HttpProvider) {
     this.privateKey = crypto.hexToBytes(privateKey);
     this.publicKey = crypto.secretKeyToPublicKey(this.privateKey);
-    const provider = new EthjsHttpProvider(process.env.JSON_RPC_URL);
     this.ethstream = new Ethstream(provider, {
       onAddBlock: block => this.onAddBlock(block),
       onConfirmBlock: block => this.onConfirmBlock(block)
